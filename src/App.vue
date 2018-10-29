411f629fe9a59a4cbf694b645494258c4f25fde4
sikun <arnazadiputra@gmail.com>
2018-10-29 17:25:34 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app" class="container" style="margin:auto; width:450px; margin-top: 150px">
+      <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'app'
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,11 @@
+<template>
+     <div class="container" style="margin:auto; width:450px; margin-top: 150px">
+        <h1>Ulala  ulalaa </h1>
+    </div>
+</template>
+<script>
+export default{
+}
+</script>
+<style>
+</style>
